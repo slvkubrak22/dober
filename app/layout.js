f33607ec6249fe287ce93d72a4bdabb8991e5d91
Vyacheslav Kubrak <slvkubrak@gmail.com>
2023-05-31 19:09:43 +0300

    main page firts section started

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import './globals.css'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './globals.css';
 
 export const metadata = {
   title: 'Dober',
--- app/page.js
+++ app/page.js
@@ -3,11 +3,10 @@
 
 export default function Home() {
   return (
-    <>
+    <div className="container">
       <h1>
-        Main Page
+        Платформа захищеного спілкування
       </h1>
-      
-    </>
+    </div>
   )
 }
