2a60d93c74e3a28ba6966bcd266b77d82d18739f
Vyacheslav Kubrak <slvkubrak@gmail.com>
2023-06-02 15:42:25 +0300

    advantages tablet

--- app/components/Advantages.jsx
+++ app/components/Advantages.jsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
+
 import styles from "../assets/styles/main-page.module.scss";
+
+import checkmark from "../assets/img/checkmark.png";
+import cross from "../assets/img/cross.png"
 
 function AdvantagesRow({
   description,
@@ -9,30 +14,126 @@ function AdvantagesRow({
 }) {
   return (
     <>
-      <div>{description}</div>
-      <div>{doberItem}</div>
-      <div>{signalItem}</div>
-      <div>{telegramItem}</div>
-      <div>{whatsappItem}</div>
+      <div className={styles["advantages-item"]}>
+        <p className={styles["advantages-item_text"]}>
+            {description}
+        </p>
+      </div>
+      <div className={styles["advantages-item"] + ' ' + styles["just"]}>
+        <Image src={doberItem} alt="Dober Item"/>
+      </div>
+      <div className={styles["advantages-item"] + ' ' + styles["just"]}>
+        <Image src={signalItem} alt="Ignal Item"/>
+      </div>
+      <div className={styles["advantages-item"] + ' ' + styles["just"]}>
+        <Image src={telegramItem} alt="Telegram Item"/>
+      </div>
+      <div className={styles["advantages-item"] + ' ' + styles["just"]}>
+        <Image src={whatsappItem} alt="Whatsapp Item"/>
+      </div>
     </>
   );
 }
 
 const advantagesData = [
   {
-    description: "dsfsdfsd",
-    doberItem: "dsfsdfsd1",
-    signalItem: "dsfsdfsd2",
-    telegramItem: "dsfsdfsd3",
-    whatsappItem: "dsfsdfsd4",
+    description: "Підтримка наскрізного шифрування за промовчаннням",
+    doberItem: checkmark,
+    signalItem: checkmark,
+    telegramItem: cross,
+    whatsappItem: checkmark,
   },
   {
-    description: "dsfsdfsd",
-    doberItem: "dsfsdfsd1",
-    signalItem: "dsfsdfsd2",
-    telegramItem: "dsfsdfsd3",
-    whatsappItem: "dsfsdfsd4",
+    description: "Відсутність збору особистих даних користувача з боку адміністрації месенджера",
+    doberItem: checkmark,
+    signalItem: checkmark,
+    telegramItem: cross,
+    whatsappItem: cross,
   },
+  {
+    description: "Хешує персональну інформацію (номер, список контактів, тощо)",
+    doberItem: checkmark,
+    signalItem: cross,
+    telegramItem: cross,
+    whatsappItem: cross,
+  },
+  {
+    description: "Застосовуються надійні криптографічні алгоритми",
+    doberItem: checkmark,
+    signalItem: checkmark,
+    telegramItem: checkmark,
+    whatsappItem: checkmark,
+  },
+  {
+    description: "Зберігання даних користувача ",
+    doberItem: cross,
+    signalItem: checkmark,
+    telegramItem: cross,
+    whatsappItem: cross,
+  },
+  {
+    description: "Захист листування від читання адміністрацією месенджера",
+    doberItem: checkmark,
+    signalItem: checkmark,
+    telegramItem: checkmark,
+    whatsappItem: checkmark,
+  },
+  {
+    description: "Вхід за PIN-кодом / парольною фразою",
+    doberItem: checkmark,
+    signalItem: checkmark,
+    telegramItem: checkmark,
+    whatsappItem: cross,
+  },
+  {
+    description: "Відсутність передачі особистих даних користувача державним органам безпеки",
+    doberItem: checkmark,
+    signalItem: checkmark,
+    telegramItem: cross,
+    whatsappItem: cross,
+  },
+  {
+    description: "Можливість анонімної реєстрації",
+    doberItem: checkmark,
+    signalItem: cross,
+    telegramItem: cross,
+    whatsappItem: cross,
+  },
+  {
+    description: "Можливість повного видалення облікового запису з системи",
+    doberItem: checkmark,
+    signalItem: cross,
+    telegramItem: cross,
+    whatsappItem: cross,
+  },
+  {
+    description: "Повідомлення учасників чату про зроблений скріншот листування",
+    doberItem: checkmark,
+    signalItem: cross,
+    telegramItem: checkmark,
+    whatsappItem: cross,
+  },
+  {
+    description: "Виділений сервер",
+    doberItem: checkmark,
+    signalItem: cross,
+    telegramItem: cross,
+    whatsappItem: cross,
+  },
+  {
+    description: "Чи рекомендується застосунок для безпечної передачі повідомлень",
+    doberItem: checkmark,
+    signalItem: checkmark,
+    telegramItem: cross,
+    whatsappItem: cross,
+  },
+//   {
+//     description: "Головна причина чому застосунок не рекомендовано",
+//     doberItem: ' ',
+//     signalItem: "Порада скасувати обов’язкову вимогу для користувачів реєструватися за допомогою мобільного номера",
+//     telegramItem: "Криптографія не за замовчуванням",
+//     whatsappItem: "Названий партнером АНБ у викриттях Сноудена",
+//   },
 ];
 
 export default function Advantages() {
@@ -53,7 +154,7 @@ export default function Advantages() {
       <div
         style={{ display: "grid", gridTemplateColumns: "2fr repeat(4, 1fr)" }}
       >
-        {advantagesData.map((item, key) => {
+        {advantagesData.map((item, key) => 
           <AdvantagesRow
             key={key}
             description={item.description}
@@ -61,10 +162,9 @@ export default function Advantages() {
             signalItem={item.signalItem}
             telegramItem={item.telegramItem}
             whatsappItem={item.whatsappItem}
-          />;
-        })}
-        {/* <AdvantagesRow />
-            <AdvantagesRow /> */}
+          />
+        )}
+        
       </div>
     </section>
   );
