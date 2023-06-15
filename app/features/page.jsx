6bae689a1081e0450d7e4c3ccb2f662bebf1e36e
Vyacheslav Kubrak <slvkubrak@gmail.com>
2023-06-15 12:33:19 +0300

    features page added icons

--- app/features/page.jsx
+++ app/features/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image";
 import { useTranslation } from "react-i18next";
 import TopSection from "../components/features/TopSection";
 
@@ -7,6 +8,13 @@ import styles from "../assets/styles/features-page.module.scss";
 import Postulates from "../components/features/Postulates";
 import UseDober from "../components/UseDober";
 import Subscribe from "../components/Subscribe";
+
+import grid_bottom_item_img_1 from "../assets/img/features-page/grid_bottom_item_img_1.svg";
+import grid_bottom_item_img_2 from "../assets/img/features-page/grid_bottom_item_img_2.svg";
+import grid_bottom_item_img_3 from "../assets/img/features-page/grid_bottom_item_img_3.svg";
+import grid_bottom_item_img_4 from "../assets/img/features-page/grid_bottom_item_img_4.svg";
+import grid_bottom_item_img_5 from "../assets/img/features-page/grid_bottom_item_img_5.svg";
+import grid_bottom_item_img_6 from "../assets/img/features-page/grid_bottom_item_img_6.svg";
 
 export default function Features() {
 
@@ -24,36 +32,36 @@ export default function Features() {
                         <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                     </div>
                     <div className={styles["grid-bottom_item"]}>
-                        {/* <Image src={img} alt="info icon"/> */}
-                        <h3>Title</h3>
-                        <p>Text</p>
+                        <Image src={grid_bottom_item_img_1} alt="info icon"/>
+                        <h3>Фінансові установи</h3>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                     </div>
                     <div className={styles["grid-bottom_item"]}>
-                        {/* <Image src={img} alt="info icon"/> */}
-                        <h3>Title</h3>
-                        <p>Text</p>
+                        <Image src={grid_bottom_item_img_2} alt="info icon"/>
+                        <h3>Юридичні установи</h3>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
                     </div>
                 </div>
                 <div className={styles["grid-bottom"] + ' ' + styles["grid-bottom-2"]}>
-                <div className={styles["grid-bottom_item"]}>
-                        {/* <Image src={img} alt="info icon"/> */}
-                        <h3>Title</h3>
-                        <p>Text</p>
+                    <div className={styles["grid-bottom_item"]}>
+                        <Image src={grid_bottom_item_img_3} alt="info icon"/>
+                        <h3>Секретні огранізації</h3>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
                     </div>
                     <div className={styles["grid-bottom_item"]}>
-                        {/* <Image src={img} alt="info icon"/> */}
-                        <h3>Title</h3>
-                        <p>Text</p>
+                        <Image src={grid_bottom_item_img_4} alt="info icon"/>
+                        <h3>Дистриб’ютори</h3>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
                     </div>
                     <div className={styles["grid-bottom_item"]}>
-                        {/* <Image src={img} alt="info icon"/> */}
-                        <h3>Title</h3>
-                        <p>Text</p>
+                        <Image src={grid_bottom_item_img_5} alt="info icon"/>
+                        <h3>Виробництва</h3>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
                     </div>
                     <div className={styles["grid-bottom_item"]}>
-                        {/* <Image src={img} alt="info icon"/> */}
-                        <h3>Title</h3>
-                        <p>Text</p>
+                        <Image src={grid_bottom_item_img_6} alt="info icon"/>
+                        <h3>Громадські огранізації</h3>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
                     </div>
                 </div>
             </section>
