e48a61a541e965a807483a5dd541c27a33b11801
Vyacheslav Kubrak <slvkubrak@gmail.com>
2023-07-14 12:31:35 +0300

    отправка письма 4

--- app/lib/mailer.js
+++ app/lib/mailer.js
@@ -1,30 +1,3 @@
-// const nodemailer = require("nodemailer");
-// const senderEmail = "slvkubrak@gmail.com";
-
-// export const transporter = nodemailer.createTransport({
-//   service: "Gmail",
-//   // host: 'smtp.gmail.com',
-//   port: "465",
-//   secure: true,
-//   auth: {
-//     user: senderEmail,
-//     pass: ",fnzflvby1966",
-//   },
-// });
-
-// const nodemailer = require("nodemailer");
-// const senderEmail = "sale@dreamxweb.com";
-
-// export const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   port: "465",
-//   secure: true,
-//   auth: {
-//     user: senderEmail,
-//     pass: "rmquowjmkewebriq",
-//   },
-// });
-
 const nodemailer = require("nodemailer");
 const senderEmail = "webrequest@dober.chat";
 
@@ -32,7 +5,7 @@ export const transporter = nodemailer.createTransport({
   // service: "gmail",
   host: "smtp.ionos.com",
   port: "465",
-  secure: false,
+  secure: true,
   auth: {
     user: senderEmail,
     pass: "Str0ngP@ssw0rdforWebRequests!",
